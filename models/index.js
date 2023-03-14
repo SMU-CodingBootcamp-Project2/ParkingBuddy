2f2648e3e79f48148f0e3589abad6ecd57df500b
Christopher Baldock <christophermbaldock@gmail.com>
2023-03-13 21:29:49 -0500

    Adds to the index model

--- models/index.js
+++ models/index.js
@@ -1,7 +1,19 @@
 const Resident = require('./Resident');
 const Lot = require('./Lot');
+const User = require('./User')
+
+Resident.belongsTo(Lot, {
+    foreignKey: 'lot_id',
+});
+Lot.hasMany(Resident, {
+    foreignKey: 'lot_id',
+});
+User.hasOne(Resident, {
+    foreignKey: 'user_id',
+});
+Resident.belongsTo(User, {
+    foreignKey: 'user_id',
+});
 
 
-
-
-module.exports = { Resident, Lot}
+module.exports = { Resident, Lot, User}
