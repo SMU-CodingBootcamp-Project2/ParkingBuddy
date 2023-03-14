a073224d0017fc82d9defbbc01ce5b7a5f30545f
anuffisenough <uffmanch@gmail.com>
2023-03-14 14:53:22 -0600

    adds handlebar files for different pageviews

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,4 +4,12 @@ router.get('/', async (req, res) => {
     res.render('homepage');
 });
 
+router.get('/user', async (req, res) => {
+    res.render('user');
+});
+
+router.get('/admin', async (req, res) => {
+    res.render('admin');
+});
+
 module.exports = router;
