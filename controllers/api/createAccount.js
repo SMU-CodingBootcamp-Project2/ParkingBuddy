be54d760d507b46739f083b4e27da0ec8e4cc2ed
Christopher Baldock <christophermbaldock@gmail.com>
2023-03-16 14:32:35 -0500

    adds post requests for the create account route

--- controllers/api/createAccount.js
+++ controllers/api/createAccount.js
@@ -1,2 +1,28 @@
+const router = require('express').Router();
+const { Resident, User } = require('../../models');
 
+router.post('/createaccount', async (req, res) => {
+    try {
+        const userData = await User.create({
+            email: req.body.username,
+            password: req.body.password,
+        });
+        const residentData = await Resident.create({
+            first_name: req.body.first_name,
+            last_name: req.body.last_name,
+            apartment_number: req.body.apartment_number,
+            car_make: req.body.car_make,
+            car_model: req.body.car_model,
+            car_color: req.body.car_color,
+            license_plate: req.body.license_plate,
+        });
+        req.session.save(() => {
+            req.session.loggedIn = true;
 
+            res.status(200).json(userData, residentData)
+        })
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
