11f2ccf0770c19129937c96b300ca144f8caa557
Sealcakes <taylorcseal@gmail.com>
2023-03-15 21:20:54 -0500

    adds passport user validation via email, fixes routes, adds session data

--- controllers/api/auth.js
+++ controllers/api/auth.js
@@ -2,31 +2,40 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
-const crypto = require('crypto');
 const { User } = require('../../models');
 
-passport.use(new LocalStrategy.Strategy({}, async (username, password, done) => {
+passport.use(new LocalStrategy.Strategy({usernameField: 'email'}, async (username, password, done) => {
     try {
         const user = await User.findOne({
             where: {
-                username: username
+                email: username
             }
         })
         console.log(user);
+        return done(null, user);
     } catch (error) {
-        done(error);
+        return done(error);
     }
 }));
 
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
 
-router.get('/login', async (req, res) => {
+passport.deserializeUser(function(user, done) {
+    done(null, user);
+})
+
+router.get('/', async (req, res) => {
     res.render('login');
 });
 
-router.post('/login/password', passport.authenticate('local', {
-    successRedirect: '/',
+router.post('/', passport.authenticate('local', {
+    // successRedirect: '/user',
     failureRedirect: '/login'
-}));
+}), (req, res) => {
+    res.redirect('/user');
+});
 
 
 module.exports = router;
