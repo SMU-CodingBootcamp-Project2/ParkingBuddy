3f5a06176be501b45a92009eeab6f76c6a72f22f
Christopher Baldock <christophermbaldock@gmail.com>
2023-03-16 21:06:02 -0500

    adds a method to decrement values if parked

--- models/Lot.js
+++ models/Lot.js
@@ -1,7 +1,24 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Lot extends Model {}
+class Lot extends Model {
+    async parkCar() {
+    if (this.resident_spots === 0){
+        throw new Error('No available resident spots left');
+    }
+    if (this.guest_spots === 0) {
+        throw new Error('No available guest spots left');
+    }
+        // decrement available resident spots
+        this.lot.resident_spots -= 1;
+        // decrement available guest spots
+        this.lot.guest_spots -= 1;
+        // decrement total parking
+        this.lot.total_parking -= 1;
+        // save the updated lot
+        await this.save();
+    }
+}
 
 Lot.init(
     {
@@ -11,23 +28,21 @@ Lot.init(
             primaryKey: true,
             autoIncrement: {
                 startAt: 1,
+                increment: 1,
                 endAt: 100
             }
         },
         total_parking: {
             type: DataTypes.INTEGER,
             allowNull: false,
-            defaultValue: 100,
         },
         resident_spots: {
             type: DataTypes.INTEGER,
             allowNull: false,
-            defaultValue: 60,
         },
         guest_spots: {
             type: DataTypes.INTEGER,
             allowNull: false,
-            defaultValue: 40,
         },
     },
     {
