d649aed3d44b6750ef45e1567c1b3dac4aafddb3
Christopher Baldock <christophermbaldock@gmail.com>
2023-03-17 16:25:09 -0500

    Adds admin route and deletes unecessary fields from lot model

---
+++ controllers/api/admin.js
@@ -0,0 +1,45 @@
+const router = require('express').Router();
+const { Lot, Resident, User } = require('../../models');
+
+router.get('/admin', async (req, res) => {
+    if(!req.sessions.has_admin) {
+        res.redirect('./user');
+    } else {
+        try {
+            const adminData = await User.findByPk(req.params.has_admin, {
+                include: [
+                    {
+                        model: Resident,
+                        attributes: [
+                            'first_name',
+                            'last_name',
+                            'apartment_number',
+                            'license_plate',
+                            'lot_id'
+                        ]
+                    }, 
+                    {
+                        model: Lot,
+                        attributes: [
+                            'available_spots',
+                            'is_resident'
+                        ]
+                    },
+                ],
+                attributes: ['email']
+            });
+
+            const admin = adminData.get({ plain: true });
+            const userData = await User.findByPk(admin.has_admin, {
+                attributes: ['email']
+            });
+            const user = userData.get({ plain: true });
+            res.render('admin', {user, admin, loggedIn: req.session.loggedIn});
+        } catch (err) {
+            console.log(err);
+            res.status(500).json(err);
+        };
+    };
+});
+
+module.exports = router;
