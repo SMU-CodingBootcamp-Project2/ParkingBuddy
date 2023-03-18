062b8d6f2c549b6b21b7b5fbf01089df9b01fd89
Christopher Baldock <christophermbaldock@gmail.com>
2023-03-18 10:17:39 -0500

    changes admin route from seeing only their info to seeing everyones

--- controllers/api/admin.js
+++ controllers/api/admin.js
@@ -2,43 +2,59 @@ const router = require('express').Router();
 const { Lot, Resident, User } = require('../../models');
 
 router.get('/admin', async (req, res) => {
-    if(!req.sessions.has_admin) {
-        res.redirect('./user');
-    } else {
-        try {
-            const adminData = await User.findByPk(req.params.has_admin, {
-                include: [
-                    {
-                        model: Resident,
-                        attributes: [
-                            'first_name',
-                            'last_name',
-                            'apartment_number',
-                            'license_plate',
-                            'lot_id'
-                        ]
-                    }, 
-                    {
-                        model: Lot,
-                        attributes: [
-                            'available_spots',
-                            'is_resident'
-                        ]
-                    },
-                ],
-                attributes: ['email']
-            });
+    try {
+        const adminData = await User.findByPk(req.sessions.id, {
+            include: [
+                {
+                    model: Resident,
+                    attributes: [
+                        'first_name',
+                        'last_name',
+                        'apartment_number',
+                        'license_plate',
+                        'lot_id'
+                    ]
+                },
+                {
+                    model: Lot,
+                    attributes: [
+                        'available_spots',
+                    ]
+                },
+            ],
+            attributes: ['email']
+        });
 
-            const admin = adminData.get({ plain: true });
-            const userData = await User.findByPk(admin.has_admin, {
-                attributes: ['email']
-            });
-            const user = userData.get({ plain: true });
-            res.render('admin', {user, admin, loggedIn: req.session.loggedIn});
-        } catch (err) {
-            console.log(err);
-            res.status(500).json(err);
-        };
+        const admin = adminData.get({ plain: true });
+        const lotData = await Lot.findAll({
+            attributes: ['available_spots'],
+            raw: true
+        });
+        const residentData = await Resident.findAll({
+            include: [
+                {
+                    model: Lot,
+                    attributes: ['available_spots']
+                }
+            ],
+            attributes: [
+                'first_name',
+                'last_name',
+                'apartment_nubmer',
+                'license_plate'
+            ],
+            raw: true
+        });
+        const residents = residentData.map((resident) => {
+            const lot = resident.Lot;
+            delete resident.Lot;
+            resident.available_spots = lot.available_spots;
+            return resident;
+        });
+        res.render('admin', { admin, lotData, residents, loggedIn: req.session.loggedIn });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
     };
 });
 
